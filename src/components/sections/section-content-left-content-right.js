e264a2352b2f73442f9ac6b785796c54f15825b9
Alexander Fountain <alexander@digett.com>
2019-04-29 16:23:31 -0500

    showdown and styled components

---
+++ src/components/sections/section-content-left-content-right.js
@@ -0,0 +1,31 @@
+import React from "react";
+import styled from "styled-components";
+import Container from "../../layout/container";
+import showdown from "showdown"
+import * as variable from '../../variables';
+
+const converter = new showdown.Converter()
+
+const SectionContentLeftContentRightStyle = styled.section`
+    display:flex;
+    justify-content:space-between;
+    .content-left{
+        flex-basis:50%;
+    }
+    .content-right{
+        flex-basis:50%;
+    }
+`;
+
+
+
+const SectionContentLeftContentRight = ({object}) => {
+return(
+<SectionContentLeftContentRightStyle className="section-content-left-content-right">
+<div className ="content-left" dangerouslySetInnerHTML={{ __html: converter.makeHtml(object.leftmarkdown) }} />
+<div className ="content-right" dangerouslySetInnerHTML={{ __html: converter.makeHtml(object.rightmarkdown) }} />
+</SectionContentLeftContentRightStyle>
+)
+}
+
+export default SectionContentLeftContentRight 
